e2d7bba8ef757e36397f0fcced0f8a54f98d7435
Harshegss <harshegss@gmail.com>
2023-06-27 16:21:37 +0530

    mkmk

--- index.js
+++ index.js
@@ -21,16 +21,11 @@ con.connect((err) => {
   // console.log('Connected to the database.');
 });
 
-let date = new Date();
-
 app.get("/items", (req, res) => {
-  const x = date.getTime();
-
   con.query("SELECT * FROM test", (error, results) => {
     if (error) throw error;
     res.status(200).json(results);
   });
-  const y = date.getTime();
 });
 
 app.listen(8080);
