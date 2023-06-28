b18fb55ee10b6e0b21e3d8f28f03c1a1f566ca79
Harshegss <harshegss@gmail.com>
2023-06-28 12:26:01 +0530

    aa

--- index.js
+++ index.js
@@ -20,11 +20,26 @@ con.connect((err) => {
   console.log('Connected to the database.');
 });
 
-app.get("/items", (req, res) => {
-  con.query("SELECT * FROM test", (error, results) => {
+// app.get("/items", (req, res) => {
+//   res.send('sdsd')
+//   // con.query("SELECT * FROM test", (error, results) => {
+//   //   if (error) throw error;
+//   //   res.status(200).json(results);
+//   // });
+// });
+app.route('/items')
+.get((req, res) => {
+      con.query("SELECT * FROM test", (error, results) => {
     if (error) throw error;
     res.status(200).json(results);
   });
-});
-
+  })
+  .post((req, res) => {
+    res.send('Add a book')
+  })
+  .put((req, res) => {
+    res.send('Update the book')
+  })
+  
 app.listen(8080);
+
